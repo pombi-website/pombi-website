158280b726d02472ae12834abc630f7d64e7679a
jeffrymahbuubi <jeffryaun@gmail.com>
2023-10-28 09:22:40 +0800

    feat: display product

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,10 +1,27 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return null;
+  const products = await prisma.product.findMany({});
+  return json({ products });
 };
 
 export default function ProductsRoute() {
-  return <div>Products</div>;
-}
+  const { products } = useLoaderData<typeof loader>();
 
+  return (
+    <div>
+      <h1>Semua Produk</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>Rp {product.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
