b3b6338060123d9320710fff7bcbb0934ef7d1e1
jeffrymahbuubi <jeffryaun@gmail.com>
2023-11-11 13:55:12 +0800

    feat: styling product page

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -12,16 +12,29 @@ export default function ProductsRoute() {
   const { products } = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <h1>Semua Produk</h1>
-      <ul>
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-2xl font-bold mb-8">Semua Produk</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
-          <li key={product.id}>
-            <h2>{product.name}</h2>
-            <p>Rp {product.price}</p>
-          </li>
+          <div
+            key={product.id}
+            className="border rounded-lg overflow-hidden shadow-lg bg-white"
+          >
+            <img
+              src={product.imageURL}
+              alt={product.name}
+              className="w-full h-64 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-lg font-semibold">{product.name}</h2>
+              <p className="text-xl text-blue-600">Rp {product.price}</p>
+              <p className="text-gray-700 text-sm mt-2">
+                {product.description}
+              </p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
