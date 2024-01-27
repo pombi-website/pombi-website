c3bcebd6fe84612c68c94332c966b1889eaffdac
jeffrymahbuubi <jeffryaun@gmail.com>
2024-01-27 23:10:38 +0800

    feat: new logo pombi and favicon

--- app/components/layout/navigation.tsx
+++ app/components/layout/navigation.tsx
@@ -1,8 +1,18 @@
+import { Link } from '@remix-run/react';
+
 export function Header() {
   return (
     <>
       <header>
-        <h1>Header</h1>
+        <Link to={`/`}>
+          <span>
+            <img
+              src="/assets/logo-pombi.png"
+              alt="logo-pombi"
+              className="w-24"
+            />
+          </span>
+        </Link>
       </header>
     </>
   );
