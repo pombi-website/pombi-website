e81236db7412059aa59baf58cd9661313aa2aa90
jeffrymahbuubi <jeffryaun@gmail.com>
2024-01-27 22:34:51 +0800

    refactor: layout components

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -1,5 +1,5 @@
-import { Header } from './header';
 import { Footer } from './footer';
+import { Header } from './navigation';
 
 interface Props {
   children: React.ReactNode;
---
+++ app/components/layout/navigation.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+      </header>
+    </>
+  );
+}
