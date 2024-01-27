6b942be09597058a3840842d6024d9cb7249cb12
jeffrymahbuubi <jeffryaun@gmail.com>
2024-01-27 22:34:24 +0800

    feat: install fontsource, add font Monserrat

---
+++ app/configs/fonts.ts
@@ -0,0 +1,9 @@
+/**
+ * EDITME: Fonts Config
+ */
+
+import headingFontStyles from '@fontsource-variable/montserrat/wght.css';
+
+export const fontLinks = [
+  { rel: 'stylesheet', href: headingFontStyles },
+];
