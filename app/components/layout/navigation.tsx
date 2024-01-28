314e5876b52772ad5c70694c2bec0d92e634e76f
jeffrymahbuubi <jeffryaun@gmail.com>
2024-01-28 13:22:11 +0800

    feat: add link to navbar

--- app/components/layout/navigation.tsx
+++ app/components/layout/navigation.tsx
@@ -3,16 +3,28 @@ import { Link } from '@remix-run/react';
 export function Header() {
   return (
     <>
-      <header>
-        <Link to={`/`}>
-          <span>
-            <img
-              src="/assets/logo-pombi.png"
-              alt="logo-pombi"
-              className="w-24"
-            />
-          </span>
-        </Link>
+      <header className="lg:px-20 flex items-center justify-between">
+        <div>
+          <Link to={`/`}>
+            <span className="flex flex-row items-center gap-6">
+              <img
+                src="/images/logo-pombi.png"
+                alt="logo-pombi"
+                className="w-24"
+              />
+              <h1 className="font-heading font-bold text-xl text-pink-500">
+                Pombi Baju Anak
+              </h1>
+            </span>
+          </Link>
+        </div>
+        <div>
+          <Link to={'/products'}>Produk Kami</Link>
+        </div>
+        <div>
+          <Link to={'/'}>Akun</Link>
+          <Link to={'/'}>Keranjang</Link>
+        </div>
       </header>
     </>
   );
